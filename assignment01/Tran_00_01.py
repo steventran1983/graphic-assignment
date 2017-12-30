f8b68780ef9da31c6fd018fcaefeb343c21f09b2
System Administrator <root@thangs-mbp.attlocal.net>
2017-12-30 01:52:55 -0600

    add folder and file Tran_00_01

---
+++ assignment01/Tran_00_01.py
@@ -0,0 +1,19 @@
+# Tran, Thang
+# 1001-233-804
+# 2017-08-31
+# Assignment_00_01
+
+import tkinter as tk
+import Tran_00_02 as k02  # This module includes all the widgets
+import Tran_00_03 as k03  # This module includes graphic components
+
+
+def close_window_callback(root):
+    if tk.messagebox.askokcancel("Quit", "Do you really wish to quit?"):
+        root.destroy()
+
+ob_root_window = tk.Tk()
+ob_root_window.protocol("WM_DELETE_WINDOW", lambda root_window=ob_root_window: close_window_callback(root_window))
+ob_world = k03.cl_world()
+k02.cl_widgets(ob_root_window, ob_world)
+ob_root_window.mainloop()
